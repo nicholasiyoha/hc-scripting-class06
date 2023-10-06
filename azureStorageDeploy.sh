45e6e9522b2a0f61efc5aab1e0491e9996604af8
nicholasiyoha <nicholasiyoha100@gmail.com>
2023-10-06 11:38:21 +0100

    add

--- azureStorageDeploy.sh
+++ azureStorageDeploy.sh
@@ -1,17 +1,19 @@
-# Define variables for the deployement of azure storage account
-$resourceGroupName = "nickomo-rg"
+# Define variables for the deployement of azure storage account using power powershell
+$resourceGroupName = "test-rg1"
 $storageAccountName = "nickomostorage"
 $location = "eastus"
 $storageSku = "Standard_LRS"  # Change this to the desired SKU
 $kind = "StorageV2"  # Change this to the desired kind (e.g., StorageV2, BlobStorage, etc.)
 
 # Create a new resource group
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+ New-AzResourceGroup create
+ -Name $resourceGroupName 
+ -Location $location
 
 # Create a new storage account
-New-AzStorageAccount
+ New-AzStorageAccount create
  -ResourceGroupName $resourceGroupName
  -Name $storageAccountName 
--SkuName $storageSku 
--Kind $kind 
--Location $location
+ -SkuName $storageSku 
+ -Kind $kind 
+ -Location $location
